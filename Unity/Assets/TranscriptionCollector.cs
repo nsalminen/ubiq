b9ebc353fe0e3fadfb3f24449de04603130aa000
Nels Numan <n.salminen@me.com>
2023-01-06 12:59:49 +0100

    Add Unity changes for previous commit

--- Unity/Assets/TranscriptionCollector.cs
+++ Unity/Assets/TranscriptionCollector.cs
@@ -7,6 +7,7 @@ using Ubiq.Messaging;
 using Ubiq.Logging.Utf8Json;
 using Ubiq.Rooms;
 using System;
+using System.Text;
 
 [NetworkComponentId(typeof(TranscriptionCollector), ComponentId)]
 public class TranscriptionCollector : MonoBehaviour, INetworkComponent
@@ -20,7 +21,8 @@ public class TranscriptionCollector : MonoBehaviour, INetworkComponent
     private struct Message
     {
         public string type;
-        public string args;
+        public string peer; // TODO: implement the source peer of this text
+        public string data;
     }
 
     // Start is called before the first frame update
@@ -37,13 +39,6 @@ public class TranscriptionCollector : MonoBehaviour, INetworkComponent
 
     public void ProcessMessage(ReferenceCountedSceneGraphMessage data)
     {
-        Debug.Log("TranscriptionCollector.ProcessMessage");
-        // var message = data.FromJson<Message>();
-        // var container = JsonUtility.FromJson<Message>(message.ToString());
-        // Debug.Log(JsonSerializer.Deserialize<Message>(message.ToString()));
-        // var container = JsonUtility.FromJson<Message>(message.ToString());
-        Debug.Log(data.ToString());
-        // Debug.Log(container.args);
-        // var message = new Message(message);
+        Debug.Log(data.FromJson<Message>().data);
     }
 }
