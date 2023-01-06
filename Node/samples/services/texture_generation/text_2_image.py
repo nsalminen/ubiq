00fa3f28358489091dd28a62b541348f4966ab91
Daniele Giunchi <dannox@gmail.com>
2023-01-06 15:49:13 +0000

    fix python issue

--- Node/samples/services/texture_generation/text_2_image.py
+++ Node/samples/services/texture_generation/text_2_image.py
@@ -6,9 +6,16 @@ import os
 import sys
 import json
 
+
+def generateTextureFromPrompt(pipe, generator, prompt):
+    image = pipe(prompt, guidance_scale=5.5, num_inference_steps=15, generator=generator).images[0]
+    md5_name = hashlib.md5(image.tobytes()).hexdigest()
+    image.save(os.path.join("outputs", md5_name + ".png"))
+    
+    print(md5_name)
+
 def recognize_from_stdin():
     print("Start to generate textures...")
-    output = "outputs/"
     pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", revision="fp16", torch_dtype=torch.float16)
     pipe.to("cuda")
     generator = torch.Generator("cuda").manual_seed(1024)
@@ -18,21 +25,15 @@ def recognize_from_stdin():
     while not done:
         try:
             line = sys.stdin.buffer.readline()
-            data = bytes(json.loads(line)["data"])
-            if len(data) == 0:
-                break
-            generateTextureFromPrompt(line)
+            if len(line) == 0:
+                continue
+            generateTextureFromPrompt(pipe, generator, str(line))
         except KeyboardInterrupt:
             break
 
 recognize_from_stdin()
 
 
-def generateTextureFromPrompt(prompt):
-    image = pipe(prompt, guidance_scale=5.5, num_inference_steps=10, generator=generator).images[0]
-    md5_name = hashlib.md5(image)
-    image.save(md5_name + ".png")
-    
-    print(md5_name)
+
     
     
