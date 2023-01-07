2fa3e4a876913e95a6429a893d681fea1f7064e2
Daniele Giunchi <dannox@gmail.com>
2023-01-07 12:31:36 +0000

    added chatgpt server

---
+++ Unity/Assets/TextGenerationCollector.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Ubiq.Networking;
+using UnityEngine;
+using Ubiq.Dictionaries;
+using Ubiq.Messaging;
+using Ubiq.Logging.Utf8Json;
+using Ubiq.Rooms;
+using System;
+using System.IO;
+using System.Text;
+
+[NetworkComponentId(typeof(TextGenerationCollector), ComponentId)]
+public class TextGenerationCollector : MonoBehaviour, INetworkComponent
+{
+    public const ushort ComponentId = 96;
+    public NetworkId networkId = new NetworkId(96);
+    private NetworkContext context;
+    public UnityEngine.UI.Text genText;
+
+    [Serializable]
+    private struct Message
+    {
+        public string type;
+        public string peer; // TODO: implement the source peer of this text
+        public string data;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        context = NetworkScene.Register(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ProcessMessage(ReferenceCountedSceneGraphMessage data)
+    {
+        string str = data.FromJson<Message>().data.ToString().Trim('\r', '\n');
+        if(str.Length > 0)
+        {
+            Debug.Log(str);
+            genText.text = str;
+        }
+        
+    }
+}
