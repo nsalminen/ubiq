cc328f1b5491b15a9eae397fa3850a465bc9393b
Nels Numan <n.salminen@me.com>
2023-01-17 11:08:32 +0000

    Fix some paths after refactor

--- Node/apps/texture_generation/app.js
+++ Node/apps/texture_generation/app.js
@@ -1,17 +1,4 @@
-// The LogCollectorService sample creates a programmatic peer that joins a room
-// and records all Experiment Log Events (0x2) it encounters.
-//
-// To achieve this, we first create a NetworkScene (the Peer), and create a
-// connection for it to the server (which is specified here as Nexus). Then we
-// add a RoomClient and LogCollector component(s). These join a room and recieve
-// log messages.
-//
-// The LogCollector uses the Id of the peers to decide where to write the events.
-// It creates new files on demand, and closes them when the corresponding Peer
-// has left the room.
-
-// Import Ubiq types
-const { NetworkScene, RoomClient, LogCollector, UbiqTcpConnection } = require("../../../ubiq");
+const { NetworkScene, RoomClient, LogCollector, UbiqTcpConnection } = require("../../ubiq");
 const fs = require("fs");
 const { TranscriptionService } = require("../../services/speech_to_text/service");
 const { TextureGenerationService } = require("../../services/image_generation/service");
