636976a824654eb653c86fc148b3a89aa882fd8a
Daniele Giunchi <dannox@gmail.com>
2023-01-19 12:36:55 +0000

    change lib and fix the script for chatGPT example

--- Node/services/text_generation/text_2_chatgpt.py
+++ Node/services/text_generation/text_2_chatgpt.py
@@ -1,8 +1,8 @@
-# from https://github.com/acheong08/ChatGPT
-# go here https://chat.openai.com/api/auth/session , in cookies and get the value of __Secure-next-auth.session-token
+# from https://github.com/acheong08/ChatGPT-lite
+# go here install and put the token you got from your browser session
 
-import selenium.webdriver 
-from revChatGPT.ChatGPT import Chatbot
+import asyncio
+from ChatGPT_lite.ChatGPT import Chatbot
 import sys
 
 
@@ -14,36 +14,42 @@ import sys
 # }
 
 
-import selenium.webdriver 
-from revChatGPT.ChatGPT import Chatbot
-
 queue = []
 chatbot = None
+loop = None
 busy = False
 done = False
 
+#that is the cooke to replace if expired , take it from the browser In Chrome is F12 , go to Application Cookies , name of the cookie"__Secure-next-auth.session-token"
+cookie = "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..KOhhwEVmbqrVabKd.8optlteTo8oWqqtRv4_uewSxTab9jZFpiztmzl_yOt6ksZXF_vpQQDrnZNPBbuhL8HhstNyf-0zS12k86x02Laf_2flWbO0t5Zg08YpQ7VEkWA3k1e_2X8bv1HpGNGZZtBFKqkQ4Uu_Mq3Gr4SF1-muGEkRKxY8nPsYgX5-Mn1bSoMUH6A3yQgC9ccy0Yfz30_JGwzXFWThQl58_f9Vs7nT-69TGF7f08c-yfd-4plZ4bk-HKnxO26xleyaB7lb5R9R7DV9xZyUgYsP24Gt_nvuK2XYvIlUUqXnhRF6OzCwzGMxo3Ww684dQ8aEeGUTaw2gRBxwd9_J18sYcbeJ9H5bujVn3URAfl8JslM_GZCiWfzLRBU_Eo9XbnEJx8UUOIHbBC51jaqXS9NNmtwf17EMWzLaZ7bkaMBKUsbElMqoQsHbv9KhT1QKTrl8mfoIh7FBR3MNXSai71E19BQ9vS9nLSjuCCBH2CUd4TahOMCi8-G4vxNd0XOvYbLTb8Tw6fDwxwsRCq5sehab0i9d4Bc3-y-THutC8qN44p2QAK1riPpJ8C9KtE7iMswZjwsV_nuOQg7wPy8-oar1vV71UJq40nbV1khQHfW8WcYt56wlu7iCZJlASEYVpsYBbs4gBDDCnEPUtcqLT9jpMqMD9zKNYQdAySz3Qu1ya3OLBjr3i3AHzxfrKInRrmJluucSY_l9_ls1jEOjuhwRpP5Uye7RGvHLZsnwXgIKZh-4bcyk1LQDws7dXNgYEcpCtPVNBGc9XTOOlKqxZk_YuvFF6_rqI87V58bgaZVuAOy6XyfmsGY2iTU5eCCBr8LVHZYDzL68qnxVbUTHNWDAhpRfohwuKmnylOrjNBqHD8fz69cnyZ4yyPSSOp-aBS6SxFihgfewjfCxlmh-JlwvRNoSz-QNTh0pBliH-0BcXWTdJSMe3TpWCRZT5ecIgy5knU74XgC2i7wxJzhzX863Psoh3HjzYOcWF0r6gmgfTUbFizw9s2h0xD3lOst3ZDIja7JCDEKbVYMLkKFqnyXp083lRb1vVP-iEgf6YeVgUQE0s03nG-YVDMKQBMZ52JTSWkWiz64cqe8_n0g-7vZvXYYi2umE7lGBcWrvyfReIzm867hbV5_25FjYweILWDkLqeE6jJCZX7ZZIlR9tuQat-XQ8z8Fw9mYVdu4pZ7wNupK7D1vY5J897MdWI6i2pHJHeptqfmxDddtMSjauy7sGZkEw8K38MCcLAhPBCn30-qryrmsKo_80KJQw_8CFzK4TDPFKy61cFCUsE1Jbtiql-buN6B8N7asWOEFhmj082sPTN1hmnsw5BJ8RNWF2SmNCb5ODD-RHOzMBZABUrVSt05hqVi5ySgqBOYVNc_GwL6pIi3S-zB1-SgsfH7OG6Iz50A7yfoIU6Tdbk49vBVU3WmorKyFxEmkBHncHwz_wJJHaxS02lfgcgQTm5Uq_eqHhZWuR8qIu4WjmK5jHaHGUv1-k_0INxvyWGs7QtsSNwQODzeuRZ4xq94s6WkBlwqGVqDoq_WulDn4WKxqZ0Vc-h_N6cwNh1iSiefANHm6aN4rFcS59hw6PsuGKEjhQ7n2_jluA_rwXDlhfbmr_H8zgemWfxjoYIyBYONsGDogw7r4qr0WP4U840bmihPvvw98ZtMxyNghLBJMIwD_vSiGQ9IWcxdyPKvdE_4KyH8HwbXZdu26z3z4CNtHSFJfB0JpnQi2CIVIacppBIJn3Cf2CPHc4eX4FE-ioGbsnZpxHa65xWlFs4c8OSTAbyTe6GujO1JUHTk3ImQgt4Hv0vCEkAjzSUCwtWLmxETkC7lWRtInymlayW0qr_GF4vGI4cnFFZp--GNOse2FMPcGzl3L0ZXTpQ6dTkzDylrS-yp947F7qGA_4DZVgvKgrRk3TV7q84C4VevY2ee_jTjRDlRvkATs7-sRx8ycu2pqCKvtbfwW8qbFc2IQ92LiUa9r2pkCUwcOOLLEAmaZvUxP21TmpMtgT7_g0xaRjhIvNPp4MHIr6UwjCuLAly7OeEEIUVLd7SHNPkirn-o2J9knJAdJnnfKnGr4EKrbppNs9-nmwKnNrhm4m89VZA6fpWHBMESJDwi__5hH4fDogCU5XY8QauHpoA6QBS1-eGmOhuPujjsCrr6AtmkrgnKGssO8ZMiJmByuZgfBylauMtZ3BEHx8rl3y7OPoCRndLgBoMR3NYtH6x_ouJ0r_avE7jz_MgltMmRIWeIL8EtSkbb9CUU75meo7HXDxbSPYAwMvuaT3JRCt.gN76xwd8HfLvmI5M35VPMA"
+
 def generateTextFromPrompt(chatbot, prompt):
-    global busy
+    global busy, loop
     prompt = prompt.decode()
     if(prompt != "" and busy == False):
         #print("-------", prompt)
         busy = True
-        response = chatbot.ask(prompt, conversation_id=None, parent_id=None) # You can specify custom conversation and parent ids. Otherwise it uses the saved conversation (yes. conversations are automatically saved)
+        response = loop.run_until_complete(chatbot.ask(prompt))
         print("{RESPONSE}:", response)
         
         busy = False
 
 def recognize_from_stdin():
-    global chatbot, done, busy
+    global chatbot, done, busy, loop
     
     if(chatbot == None):
-        chatbot = Chatbot({
-          "session_token": "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0.._zIMaJdVQbtSHDzh.kkyJo4ati-sH7aOh2k2YBAX6-RDLKg81h0oWweGHyezmLPKPanyRszM1JhkhtbhLIZvLIFsbGeJsrGe47kf0GJ39QuDh-5ZYXA4zCCpmqqjzz43y-wL1MwP7ClZSmNUFejrynGioTX5OQWAlfgW1itzfKEqNsvPpk13cT6QaKP9bxpWo9HfBFn7lQNjN7bJUXBdoBY1x42MQb4zUm3yLsxxFlq6xsu5m1vYEsBfXBbUyn_BjjuUs-l3aRnHirw2cldBaZoT9bTqOvw23Bz-QDiaS4ACKS_MfevcnxIACLhQnKAplxR4p8MpapJrRBjBz8P_zaRnZKNg_q-9DzQG2MX6k5VusoucPmkkJOw4LNKFlshpLziilCNOw-I65HVDCmG0WGqKOP2ZdynNXH7j3qEFgy1PHVWGT2e94q63ublSIL1TnrDIpyo4Xy_KtgVHVAPorKwJ0EsPEWGmHcZBGJbONcxn1DiAE-CB9PMXEyFJZ8psK-o39g-HoVNXiXS69bqAVLpYqf4rwPpkmEKf-tps_voMaJ5rL22Kwj4RZ6L3sUk4rRHDG-oTWXR8ZwZdRsVfuOYAswtybJ3zuOV3M_fMOg0OPFo7Eap48su9t1r64EH4TyoxN5MIOusaMfOx5OwBQZ7cF2UHPUIXgOVUoAxAnmgdd1P6P3Rs7obcsZXCX9t2SYANsKu5JQnHwyueMUVQlDDiF5eBU1cwmeZUxRiz1Te7_WoBVTKBQYwjYTvnkpBwtrP3tFQkKttT0sn2wFPkmCEi8dDSgqRSx8Gz2Mym7-PtfwCyOZLswyMRLWc6HYmNFl4cX4UjwCL8IRKXIV7gkeuE98D32pHvQp6P160grYOeKa369NZrXLRfkVzA4wYslxWfbOzRvtNaQfMyMuSAJXRr0Jdm2sh8vcVlLVfVkgoFSAHSzF1autHxBuVJ2ljWUsaylzNUBJwOjM6reg1W10ZAvL-MyQrF3-EIFaYVrjvEjRrpeFJHncEA5F6dbN-27viju-tRrnfn7APaKT6PQ3EgAzUi3YdZh_BvV5TeZzcx1djF3MesviimSRJ5uycmkpxmdT4I31GTJ_qqMQhU8b2dgvysp3UHERl97WkkphvbG8iUydgOCATeOHuCnkvqRwhJ_c3idjLsUekeghFO7W8puTEeCBE5UxYrwEQU2DLGM52Ssr5HdFTqLLF4W-NdtXQi0bJa5HnEjYdGTfIsfDpmVSw5mOG-HPwlbXcBhciXG1LBm5xRApWFfc8SnM9eI5-UtX16wueOZbc6ucD-HlHHxnPAghUy4WiHv0MX_Ut6zYEkf3EKCvtKov-5YKTH7m7aCF4RVds-jR86ycUc1-3OTXuzS6F1LSOIjLxzOfvEKfUdeADwCSCmjnNY_xgj4phrdD8DjP6ZEIqIc1IsCjMnc7DQ_CUAuhZB8PMeTbh6RuAFZGsnMPzGEVdPfEaLCjvh3ZQMQjTPWW9VV_2qxmbvB3sfKDMvmquzcPpXL2p7Q1ZiGdhXcnP4y3obuDkB9lT7TUo_oHxmy7gU9ZMTkAUfUOvkf1glKo8kS0fd8emeH_TetLF0NxVuLUI1_BoTDfPdxkeGxsn6drhihEcKU8hOZL_LeQ0lWpm4AyIAHxWlHfm3I92McCUln-0E81c-iToRO9maXxzXsCSsjYTsVsYIE00vc6fzHwT8xPlcQ9eA_J9PqW8d7uMV-Ca47WnzZWTG-v6lcWrl3TzMHujbJVf-bKzUYe9eD9TpHofJLLHXprYGG3Lb_sPscPVVcKH_zPMAUH0roK6cePk41Bf2gJ0Dwlvuc3VJt3DnVlf8bo6y5phxF-FZZus1av6CxmG9vAMk3jkrS4y5jWIkNVLahULxO6amy-NtwH0FeHJcweHvTV6zARwZXD2pCqwIFkaTv7dmO5-eA1HgZs1wvsgcdAEzqYnKUIVaCrWcLfbY__BsX5DCDJoqSUd25W8gyOQlyTcq0l9BF5LexXtUB3OtkqMTUqodW2WFeQsolNCsl_YXyewYSvcBj6usn_fc8Vh3rFtJsBxvezyiALiEWc2jq-T5Fhqu3hmfNue19CMibxGZEsijL6Vn6I4x-9nZ1XtKwmv8g5kGG_vjoilWxn20JD7H6hlQ-rwVCAnd7_mZMKKuOAHI7DVEJ3H8h9bCU65QtDvXoZ_lgf2ADZk6NhE-7A4O6FZtwUgKv6QXF-ZhQa7t1YDQi4AlUdugc_IILE2u-r2a4ZlOmF1FxE9-o3FU7uK6LVf43A-bbA6aq8ASdBVtIemN-gAn1diJY.2MnTx31giguW-Nss4KsyAg"
-        }, conversation_id=None, parent_id=None) # You can start a custom conversation
+        chatbot = Chatbot(cookie, "https://gpt.pawan.krd")
+        # Create loop
+        loop = asyncio.new_event_loop()
+        # Set
+        asyncio.set_event_loop(loop)
+        # Run
+        loop.run_until_complete(chatbot.wait_for_ready())
     
     # Write stdin to the stream
     while not done:
         try:
+            print("going")
             line = sys.stdin.buffer.readline()
             if len(line) == 0:
                 continue
